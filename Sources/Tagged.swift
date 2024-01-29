559e09da79c32dc58177da016cb38c2f79f70903
Frank Lehmann <frank.lehmann@bdr.de>
2024-01-29 14:14:31 +0100

    Make tagged public and add public initializers

--- Sources/Tagged.swift
+++ Sources/Tagged.swift
@@ -1,11 +1,21 @@
 import Foundation
 
-struct TaggedUtf8String: Codable {
-    let tag: UInt64
-    let value: String
+public struct TaggedUtf8String: Codable {
+    public let tag: UInt64
+    public let value: String
+    
+    public init(tag: UInt64, value: String) {
+        self.tag = tag
+        self.value = value
+    }
 }
 
-struct TaggedByteString: Codable {
-    let tag: UInt64
-    let value: [UInt8]
+public struct TaggedByteString: Codable {
+    public let tag: UInt64
+    public let value: [UInt8]
+    
+    public init(tag: UInt64, value: [UInt8]) {
+        self.tag = tag
+        self.value = value
+    }
 }
